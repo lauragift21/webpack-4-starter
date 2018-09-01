39d3d60a0d883fd5091ede98563aedbb6a67967e
lauragift21 <laurandidi21@gmail.com>
2018-09-01 21:37:19 +0100

    Add folder structure and boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = () => {
+    return (
+        <div>
+            <p>React Trial gone wrong!</p>
+        </div>
+    );
+};
+
+export default app;
+
+ReactDOM.render(<App />, document.getElementById('app'));
